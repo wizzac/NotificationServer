d313466ef4b29a252b652e127f5d2913a9238d12
wizzac <sebycuk@yahoo.com.ar>
2018-08-13 20:10:30 -0300

    USUARIOSSSSS

--- index.js
+++ index.js
@@ -8,13 +8,20 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var last_user;
+
 io.on('connection', function(socket){
-  //console.log(socket);
-  console.log("entro alguien");
-  socket.on('chat message', function(msg){
-  	console.log('interceptando: ' + msg);
-    io.emit('chat message', msg);
-  });
+  	console.log("entro alguien");
+  	socket.broadcast.emit('user connected');
+    	
+  	socket.on('chat message', function(msg,nickname){
+  	//	socket.on('send-nickname', function(nickname) {
+    		last_user=nickname;
+	//});
+  		console.log('interceptando: ' + msg);
+    	io.emit('chat message',msg,last_user);
+  	});
+
    socket.on('disconnect',function(socket){
   	console.log("se fue alguien");
   });
